a2e1786d1c528db710ec59bb407f9f523353c217
LauraPorri <laura.gallegosibarra@gmail.com>
2024-02-14 18:25:53 -0600

    Creando script app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Data_exploration
 Proyecto 5 TripleTen
+
+En este proyecto vamos a descargar una base de datos en Excel para visualización de datos, 
+podemos crear un histograma y un scatter plot.
---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+st.header('Visualizacion de datos')
+
+hist_button = st.button('Construir histograma')  # crear un botón
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig1 = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig1, use_container_width=True)
+
+scatter_button = st.button('Contruir un gráfico de dispersión')
+if scatter_button:
+
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un scatterplot
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
