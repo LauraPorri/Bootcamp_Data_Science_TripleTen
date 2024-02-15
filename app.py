d3240853642ccca79c0e2d339e64d18f64207309
LauraPorri <laura.gallegosibarra@gmail.com>
2024-02-14 18:53:04 -0600

    Cambiar botón por casillas

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv')
-st.header('Visualizacion de datos')
+st.header('Visualización de datos')
 
-hist_button = st.button('Construir histograma')  # crear un botón
+hist_button = st.checkbox('Construir histograma')  # crear un botón
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
-    fig1 = px.histogram(car_data, x="odometer")
+    fig1 = px.histogram(car_data, x="odometer", )
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig1, use_container_width=True)
 
-scatter_button = st.button('Contruir un gráfico de dispersión')
+scatter_button = st.checkbox('Contruir un gráfico de dispersión')
 if scatter_button:
 
     st.write(
